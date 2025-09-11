fef4fa3890fdef51eaf29ec9ee18f5fcf2b8927c
kruziikrel13 <dev@michaelpetersen.io>
2025-09-11 21:13:46 +1000

    VulkanContext: flesh out remainder of class variables and methods

--- include/VulkanContext.hpp
+++ include/VulkanContext.hpp
@@ -15,17 +15,32 @@ class CVulkanContext {
     void                  pickPhysicalDevice();
     void                  createLogicalDevice();
     void                  createCommandPool();
-    void                  createDescriptorSetLayout();
     void                  createComputePipeline();
+    void                  createDescriptorSetLayout();
+    void                  createDescriptorPool();
+    void                  createDescriptorSets();
+    void                  createBuffers(float* matrixA, float* matrixB, float* matrixC, int matrix_size);
+    void                  allocateBufferMemory(VkBuffer buffer, VkDeviceMemory* bufferMemory, VkDeviceSize size, VkMemoryPropertyFlags properties);
+    uint32_t              findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+    void                  recordCommandBuffer();
     void                  cleanup();
 
     VkInstance            instance;
     VkDevice              device;
-    VkQueue               computeQueue;
-    VkCommandPool         commandPool;
     VkPhysicalDevice      physicalDevice;
-    VkDescriptorSetLayout descriptorSetLayout;
+    VkQueue               computeQueue;
     uint32_t              computeQueueFamilyIndex;
+    VkCommandPool         commandPool;
+    VkPipeline            computePipeline;
+    VkPipelineLayout      pipelineLayout;
+    VkShaderModule        computeShaderModule;
+    VkCommandBuffer       commandBuffer;
+    VkDescriptorSetLayout descriptorSetLayout;
+    VkDescriptorPool      descriptorPool;
+    VkDescriptorSet       descriptorSet;
+
+    VkBuffer              bufferA, bufferB, bufferC;
+    VkDeviceMemory        bufferMemoryA, bufferMemoryB, bufferMemoryC;
 
     int                   matrixSize;
 };
