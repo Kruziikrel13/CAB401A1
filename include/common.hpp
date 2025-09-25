a18dbf37efb53f8e22c1eaffeccb1ebbccb8064d
kruziikrel13 <dev@michaelpetersen.io>
2025-09-25 16:49:05 +1000

    optimisation: define matrix size as a constexpr val

--- include/common.hpp
+++ include/common.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 namespace NCommon {
+    constexpr size_t   MATRIX_SIZE = 1024;
     std::vector<float> generateRandomMatrix(size_t size);
     void               writeToBinary(std::vector<float>& matrix, std::string filename);
 }
